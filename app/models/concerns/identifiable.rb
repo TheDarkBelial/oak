6cddca9c716f5b25b9231c41163a5e156ffc52c2
Bert McCutchen <mail@bertm.dev>
2025-12-20 16:35:53 -0500

    Fixed Turbo navigation issues when identifying models by secure tokens

--- app/models/concerns/identifiable.rb
+++ app/models/concerns/identifiable.rb
@@ -7,6 +7,12 @@ module Identifiable
     def identifies_by(attribute)
       self.identity_key = attribute
     end
+
+    # Ensures tokens generated with secure token will never start with a number.
+    # This ensures any class that are identified by tokens work properly with Turbo.
+    def generate_unique_secure_token(...)
+      "OAK#{super(...)}"
+    end
   end
 
   included do
