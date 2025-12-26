01ce3f1cb15f4f48e2bae2486cc7319767fedfa6
Bert McCutchen <mail@bertm.dev>
2025-12-26 18:24:26 -0500

    Removed unused theme variables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  DEFAULT_THEMES = %w[
-    light dark cupcake bumblebee emerald corporate
-    synthwave retro cyberpunk valentine halloween garden forest aqua lofi
-    pastel fantasy wireframe black luxury dracula cmyk autumn business acid
-    lemonade night coffee winter dim nord sunset caramellatte abyss silk
-  ].sort.freeze
-
-  def default_themes = DEFAULT_THEMES
 end
