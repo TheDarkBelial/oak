258a03af6d899312707b4f86223d2c0339c4fff1
Bert McCutchen <mail@bertm.dev>
2026-01-05 17:17:55 -0500

    Refactored and extracted color conversion into a helper

---
+++ app/helpers/color_helper.rb
@@ -0,0 +1,69 @@
+require "rmagick"
+
+# There's a lot of magic numbers in this file.
+# I don't like it, but answers are hard to come by when looking into them.
+module ColorHelper
+  def oklch_to_oklab(l:, c:, h:)
+    h_rad = (h * Math::PI) / 180
+    a_axis = c * Math.cos(h_rad)
+    b_axis = c * Math.sin(h_rad)
+
+    { l: l, a: a_axis, b: b_axis }
+  end
+
+  def oklab_to_linear_rgb(l:, a:, b:)
+    l_cube = (l + 0.3963377774 * a + 0.2158037573 * b) ** 3
+    m_cube = (l - 0.1055613458 * a - 0.0638541728 * b) ** 3
+    s_cube = (l - 0.0894841775 * a - 1.2914855480 * b) ** 3
+
+    {
+      r: +4.0767416621 * l_cube - 3.3077115913 * m_cube + 0.2309699292 * s_cube,
+      g: -1.2684380046 * l_cube + 2.6097574011 * m_cube - 0.3413193965 * s_cube,
+      b: -0.0041960863 * l_cube - 0.7034186147 * m_cube + 1.7076147010 * s_cube
+    }
+  end
+
+  def linear_rgb_to_standard_rgb(r:, g:, b:)
+    {
+      r: gamma_correct(r),
+      g: gamma_correct(g),
+      b: gamma_correct(b)
+    }
+  end
+
+  def standard_rgb_to_hex(r:, g:, b:)
+    sprintf("#%02x%02x%02x", convert_8bit(r), convert_8bit(g), convert_8bit(b))
+  end
+
+  def oklch_to_hex(l:, c:, h:)
+    oklab = oklch_to_oklab(l:, c:, h:)
+    linear_rgb = oklab_to_linear_rgb(**oklab)
+    standard_rgb = linear_rgb_to_standard_rgb(**linear_rgb)
+
+    standard_rgb_to_hex(**standard_rgb)
+  end
+
+  def parse_oklch(oklch)
+    matches = oklch.match(/oklch\s*\(\s*(?<l>[0-9.]+)%?\s+(?<c>[0-9.]+)\s+(?<h>[0-9.]+)(?:deg)?\s*\)/i)
+    raise ArgumentError, "Invalid OKLCH color" unless matches
+
+    l = matches[:l].to_f
+    c = matches[:c].to_f
+    h = matches[:h].to_f
+    l = l / 100 if oklch.include?("%")
+
+    { l:, c:, h: }
+  end
+
+  private
+
+  def gamma_correct(val)
+    return 12.92 * val if val <= 0.0031308
+
+    1.055 * (val ** (1 / 2.4)) - 0.055
+  end
+
+  def convert_8bit(val)
+    [ 0, [ 255, (val * 255).round ].min ].max
+  end
+end
--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -1,7 +1,9 @@
 require "rmagick"
+require_relative "color_helper"
 
 module IconsHelper
   include ActionView::Helpers::AssetUrlHelper
+  include ColorHelper
 
   def icon_tag(icon, **opts)
     return no_icon_tag(**opts) unless icon
@@ -39,7 +41,7 @@ module IconsHelper
   end
 
   def colorize_png(icon_variant, color = nil)
-    hex_color = oklch_to_hex(color || Current.theme.color_primary)
+    hex_color = oklch_to_hex(**parse_oklch(color || Current.theme.color_primary))
 
     Rails.cache.fetch([ icon_variant, hex_color ], expires_in: 12.hours) do
       image = Magick::Image.read(icon_asset_path(icon_variant)).first
@@ -57,58 +59,5 @@ module IconsHelper
 
   def icon_asset_path(icon_variant)
     image_path(Rails.root.join(icon_variant.path))
-  end
-
-  # I don't really like this, but I need a hex value for ImageMagick.
-  # There's far too many unexplained magic numbers.
-  # https://bottosson.github.io/posts/oklab/#oklab-implementations
-  def oklch_to_hex(oklch)
-    # Parse OKLCH string
-    match = oklch.match(/oklch\s*\(\s*([0-9.]+)%?\s+([0-9.]+)\s+([0-9.]+)(?:deg)?\s*\)/i)
-    raise ArgumentError, "Invalid OKLCH color" unless match
-
-    l = match[1].to_f
-    c = match[2].to_f
-    h = match[3].to_f
-
-    # Convert percentage to a decimal value.
-    l = l / 100 if oklch.include?("%")
-
-    # Convert OKLCH to OKLAB.
-    h_rad = (h * Math::PI) / 180
-    a = c * Math.cos(h_rad)
-    b = c * Math.sin(h_rad)
-
-    # Convert OKLAB to linear RGB.
-    l_ = l + 0.3963377774 * a + 0.2158037573 * b
-    m_ = l - 0.1055613458 * a - 0.0638541728 * b
-    s_ = l - 0.0894841775 * a - 1.2914855480 * b
-
-    l3 = l_ * l_ * l_
-    m3 = m_ * m_ * m_
-    s3 = s_ * s_ * s_
-
-    r = +4.0767416621 * l3 - 3.3077115913 * m3 + 0.2309699292 * s3
-    g = -1.2684380046 * l3 + 2.6097574011 * m3 - 0.3413193965 * s3
-    b_rgb = -0.0041960863 * l3 - 0.7034186147 * m3 + 1.7076147010 * s3
-
-    # Convert linear RGB to sRGB.
-    r = linear_to_srgb(r)
-    g = linear_to_srgb(g)
-    b_rgb = linear_to_srgb(b_rgb)
-
-    # Clamp and convert to 8-bit.
-    r = [ 0, [ 255, (r * 255).round ].min ].max
-    g = [ 0, [ 255, (g * 255).round ].min ].max
-    b_rgb = [ 0, [ 255, (b_rgb * 255).round ].min ].max
-
-    # Convert to HEX.
-    sprintf("#%02x%02x%02x", r, g, b_rgb)
-  end
-
-  def linear_to_srgb(val)
-    return 12.92 * val if val <= 0.0031308
-
-    1.055 * (val ** (1 / 2.4)) - 0.055
   end
 end
